9fa5ae922088fba015b0a16ffcd5c269c9f0cfeb
Murad <murad-arslangiraev-1994@mail.ru>
2020-06-05 17:09:38 +0300

    Подготовка проекта, пишем свои redux

--- src/native.js
+++ src/native.js
@@ -6,17 +6,31 @@ const counter = document.getElementById('counter'),
     asyncBtn = document.getElementById('async'),
     themeBtn = document.getElementById('theme');
 
+let state = 0;
+
+function render() {
+    counter.textContent = state.toString();
+}
 
 addBtn.addEventListener('click', () => {
+    state++
+    render()
 })
 
 subBtn.addEventListener('click', () => {
+    state--
+    render()
 })
 
 asyncBtn.addEventListener('click', () => {
-
+    setTimeout(() => {
+        state++
+        render()
+    }, 2000)
 })
 
 themeBtn.addEventListener('click', () => {
-    // document.body.classList.toggle('dark');
+    document.body.classList.toggle('dark');
 })
+
+render();
