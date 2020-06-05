6f14056e5f330c3944ed4b95d52631d751f8f5fb
Murad <murad-arslangiraev-1994@mail.ru>
2020-06-05 17:00:26 +0300

    сначало сделал на vanillaJS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import './styles.css'
+
+const counter = document.getElementById('counter'),
+    addBtn = document.getElementById('add'),
+    subBtn = document.getElementById('sub'),
+    asyncBtn = document.getElementById('async'),
+    themeBtn = document.getElementById('theme');
+
+let state = 0;
+
+function render() {
+    counter.textContent = state.toString();
+}
+
+addBtn.addEventListener('click', () => {
+    state++
+    render()
+})
+
+subBtn.addEventListener('click', () => {
+    state--
+    render()
+})
+
+asyncBtn.addEventListener('click', () => {
+    setTimeout(() => {
+        state++
+        render()
+    }, 2000)
+})
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+})
+
+render();
